8e55bae39cf706584f5058d1acf3f1ba8d0eae78
NEVER22LATE <servesh844@gmail.com>
2019-02-18 19:39:27 +0530

    Update FileExample1.py

--- FileExample1.py
+++ FileExample1.py
@@ -1,1 +1,13 @@
 
+
+# Reading file example
+
+try :
+	fhandle = open('file_Example.txt','r')
+except : 
+	print("file not find")	
+
+for line in fhandle :
+	line = line.rstrip()
+	print(line.upper())
+
