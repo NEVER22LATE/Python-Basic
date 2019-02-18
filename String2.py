aaa7ac560e865bbf7ec83a1ec221033b21715323
NEVER22LATE <servesh844@gmail.com>
2019-02-18 18:17:54 +0530

    Update String2.py

--- String2.py
+++ String2.py
@@ -1,1 +1,21 @@
 
+
+# Parsing an Extracting 
+
+data = " From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008"
+
+# Take the following python code that stores a string 
+
+str = 'X-DSPAM-Confidence:0.8475'
+
+# Use find and string slicing to extract the portion of the string after the
+#colon character and then use the float function to convert the extracted
+#string into a floating point number
+
+index = str.find(":")
+
+float_number = str[index+1:]
+
+fn = float(float_number)
+
+print(fn)
