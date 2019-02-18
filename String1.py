77687ae8dfcbe16fae9d1e21a83b5231ffe9c0a7
NEVER22LATE <servesh844@gmail.com>
2019-02-18 13:20:25 +0530

    Update and rename PythonDatastructure to String1.py

---
+++ String1.py
@@ -0,0 +1,22 @@
+
+# String Data Type 
+
+#Reading and converting 
+
+#Slicing String 
+
+mystring = 'This is the string we want to perform slicing'
+
+print(mystring)
+
+print(mystring[1:4])
+
+print(mystring[1::2])
+
+print(mystring[:5]) 
+
+print(mystring[1:5:2])
+
+print(mystring[::])
+
+print(mystring[1:])
